ef8230a5304207ce566ffdb63b9633cf32d8389f
LapT <LapT@LapT-PC>
2020-04-11 22:11:27 -0500

    Mi primer GIT branch Master. Con proyecto Spring boot.
    Proyecto sencillo solo muestra mensajes sencillos de cada metodo REST.
    
    GET, POST, PUT, DELETE.

---
+++ mobile-app-ws/src/main/java/com/blog/app/ws/ui/contoller/UserController.java
@@ -0,0 +1,38 @@
+package com.blog.app.ws.ui.contoller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")  //http://localhost:8080/users
+public class UserController {
+
+	@GetMapping
+	public String getUser()
+	{
+		return "metodo getUser() llamado, SE REALIZO UNA OPERACION CON EL COMANDO GET";
+		
+	}
+	
+	@PostMapping
+	public String createUser()
+	{
+		return "metodo createUser()  llamado";
+	}
+	
+	@PutMapping
+	public String updateUser()
+	{
+		return "metodo updateUser()  llamado";
+	}
+	
+	@DeleteMapping
+	public String deleteUser()
+	{
+		return "metodo deleteUser()  llamado";
+	}
+}//CLASE
